902d02897b3d66df10db5b82bc77171180e8d0d0
wnsgur9137 <wnsgur9137@icloud.com>
2024-08-20 10:00:03 +0900

    ✨ Localize Bookmark Constants

---
+++ Projects/Presentation/BookmarkPresentation/Sources/Constants/Constants+String.swift
@@ -0,0 +1,22 @@
+//
+//  Constants+String.swift
+//  BookmarkPresentation
+//
+//  Created by JunHyoek Lee on 8/20/24.
+//  Copyright © 2024 com.junhyeok.PillInformation. All rights reserved.
+//
+
+import Foundation
+
+import BasePresentation
+
+extension Constants.Bookmark {
+    static let bookmark: String = "bookmark".localized
+    static let loadErrorMessage: String = "bookmarkLoadErrorMessage".localized
+    static let saveErrorMessage: String = "bookmarkSaveErrorMessage".localized
+    static let serverErrorMessage: String = "serverErrorMessage".localized
+    static let emptyBookmark: String = "emptyBookmark".localized
+    static let emptyBookmarkDescription: String = "emptyBookmarkDescription".localized
+    static let deleteAll: String = "deleteAll".localized
+    static let askDeleteAll: String = "askDeleteAll".localized
+}
