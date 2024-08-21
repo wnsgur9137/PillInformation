bd3a4f93009bd9d609362e71d1920d48acc4a38f
wnsgur9137 <wnsgur9137@icloud.com>
2024-08-21 09:53:44 +0900

    ✨ Injection Manager project

--- Projects/Application/Sources/SceneDelegate.swift
+++ Projects/Application/Sources/SceneDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+import InjectionManager
 import KakaoLibraries
 import NotificationInfra
 
---
+++ Projects/InjectionManager/Tests/Tests.swift
@@ -0,0 +1,8 @@
+//
+//  Tests.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by JunHyoek Lee on 8/21/24.
+//
+
+import Foundation
